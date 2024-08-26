d3dcf8e3e5b60dbe8f8be096f3620f67fcbb9e4c
David Meskhishvili <dmeskh@gmail.com>
2024-08-25 23:20:00 -0700

    Finish the project

--- main.js
+++ main.js
@@ -4,12 +4,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader'); 
 
 // Declare Global Variable
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function hideLoading() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+} 
+
 // Show new quote
 function newQuote() {
+    loading();
     // Pick a random quote from quotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if author field is blank
@@ -24,12 +38,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
-
+    // Set quote, hide loader
     quoteText.textContent = quote.text;
+    hideLoading();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
